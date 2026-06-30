ae82cb5c14d0f732c0ed61de7a7da57eafd0504f
Principi-Angel <angel.principi@gmail.com>
2026-06-30 03:17:52 -0300

    functional game

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,31 +2,52 @@ import React, { useState, useEffect } from "react";
 import WordInput from "./WordInput.jsx";
 import WordChain from "./WordChain.jsx";
 import Timer from "./Timer.jsx";
-
+//import "../styles/components/GameBoard.css"
+import "../styles/game.css"
 const GameBoard = () => {
     const [chain, setChain] = useState([]);
     const [word, setWord] = useState("");
     const [score, setScore] = useState(0);
-    const [timeLeft, setTimeLeft] = useState(15);
+    const [timeLeft, setTimeLeft] = useState(null);
     const [gameOver, setGameOver] = useState(false);
 
     useEffect(() => {
-        if (gameOver) return;
+        if (timeLeft === null) return;
         if (timeLeft <= 0) {
           setGameOver(true);
           return;
         }
         const interval = setInterval(() => {
           setTimeLeft((prev) => prev - 1);
-        }, 15);
+        }, 1000);
         return () => clearInterval(interval);
     }, [timeLeft, gameOver]);
-        
+    
+    if (gameOver) {
+    return (
+      <div className="game-over-screen">
+        <h1>Fin de la partida</h1>
+        <p>Puntaje final: {score}</p>
+        <button onClick={() => {
+          // reinicio partida
+          setChain([]);
+          setWord("");
+          setScore(0);
+          setTimeLeft(null);
+          setGameOver(false);
+        }}>
+          Jugar de nuevo
+        </button>
+      </div>
+    );
+  }
 
     return (
-    <div>
+    <div className="game-board">
       <h1>Palabras Encadenadas</h1>
-      <Timer timeLeft={timeLeft} />
+      <Timer
+        timeLeft={timeLeft}
+      />
       <WordInput
         word={word}
         setWord={setWord}
@@ -36,9 +57,10 @@ const GameBoard = () => {
         setScore={setScore}
         setTimeLeft={setTimeLeft}
       />
-      <WordChain chain={chain} />
+      <WordChain
+        chain={chain}
+      />
       <p>Puntaje: {score}</p>
-      {gameOver && <p>Fin de la partida</p>}
     </div>
   );
 };
