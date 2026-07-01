48982843947794a75ca1ab97fac91c62ac019e3d
Principi-Angel <angel.principi@gmail.com>
2026-07-01 02:36:17 -0300

    add LeaderBoard

---
+++ src/components/LeaderBoard.jsx
@@ -0,0 +1,10 @@
+
+
+const Leaderboard = () => (
+  <div className="leaderboard">
+    <h1>Tabla de Puntajes</h1>
+    <p>Estos son los mejores puntajes:</p>
+  </div>
+);
+
+export default Leaderboard;
