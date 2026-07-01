397fcaf222443736f49e9eb92f10c0a996cc2c64
Principi-Angel <angel.principi@gmail.com>
2026-07-01 00:34:43 -0300

    add axios

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,3 +1,4 @@
+/*
 const API_URL = "https://word-api-hmlg.vercel.app/api";
 
 export const validateWord = async (word) => {
@@ -5,4 +6,15 @@ export const validateWord = async (word) => {
   if (!res.ok) throw new Error("Error en la validación");
   const data = await res.json();
   return data.exists;
+};
+*/
+import axios from "axios";
+
+const API_URL = "https://word-api-hmlg.vercel.app/api";
+
+export const validateWord = async (word) => {
+  const res = await axios.get(`${API_URL}/validate`, {
+    params: { word }
+  });
+  return res.data.exists;
 };
