c3c35328573fd5f9826188948441b6edfc6c6b90
Principi-Angel <angel.principi@gmail>
2026-07-01 16:19:22 -0300

    change limit to a constant

--- src/helpers/ScoreManager.jsx
+++ src/helpers/ScoreManager.jsx
@@ -1,23 +1,24 @@
 import { getScores } from "../services/StorageService";
+const LIMIT_SCORES = 5;
 
 export const isHighScore = (score) => {
-      const scores = getTopScores(5);
+      const scores = getTopScores();
       if (scores.length < 5) return true;
       const minScore = Math.min(...scores.map(s => s.score));
       return score > minScore;
 };
 
-export const getTopScores = (limit) => {
+export const getTopScores = () => {
   const scores = getScores();
   return scores
     .sort((a, b) => b.score - a.score)
-    .slice(0, limit);
+    .slice(0, LIMIT_SCORES);
 };
 
-export const trimScores = (limit) => {
+export const trimScores = () => {
   const scores = getScores();
-  if (scores.length <= limit) return;
-  const sorted = scores.sort((a, b) => b.score - a.score).slice(0, limit);
+  if (scores.length <= LIMIT_SCORES) return;
+  const sorted = scores.sort((a, b) => b.score - a.score).slice(0, LIMIT_SCORES);
   localStorage.setItem("scores", JSON.stringify(sorted));
 };
 
