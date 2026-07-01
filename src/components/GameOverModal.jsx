3e0b0649fbf1adc53e8da7468daec5a99920a927
Principi-Angel <angel.principi@gmail.com>
2026-07-01 04:01:30 -0300

    fix isHighScore

--- src/components/GameOverModal.jsx
+++ src/components/GameOverModal.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/components/GameOverModal.css";
 import { useNavigate } from "react-router-dom";
 import { isHighScore } from "../helpers/ScoreManager";
+import BackToMenuButton from "./BackToMenuButton.jsx";
 
 const GameOverModal = ({ score, wordsCount, onRestart, onSaveScore }) => {
   const [playerName, setPlayerName] = useState("");
@@ -14,39 +15,42 @@ const GameOverModal = ({ score, wordsCount, onRestart, onSaveScore }) => {
     setPlayerName("");
     navigate("/scores")
   };
+  useEffect(() => {
+    console.info("isHighScore", isHighScore(score));
+  }, []);
 
  const handleMenu = () => { navigate("/"); }; 
 
   return (
-      <div className="modal-overlay">
-        <div className="game-over-modal">
-          <h1>Fin de la partida</h1>
-          <p>Puntaje final: {score}</p>
-          <p>Cantidad de palabras: {wordsCount}</p>
+    <div className="modal-overlay">
+      <div className="game-over-modal">
+        <h1>Fin de la partida</h1>
+        <p>Puntaje final: {score}</p>
+        <p>Cantidad de palabras: {wordsCount}</p>
 
-          {isHighScore ? (
-            <>
-              <input
-                type="text"
-                placeholder="Ingresa tu nombre"
-                value={playerName}
-                onChange={(e) => setPlayerName(e.target.value)}
-              />
-              <button onClick={handleSave}>Guardar Puntaje</button>
-            </>
-          ) : (
-            <p className="no-highscore">No lograste un puntaje alto. ¡Intenta de nuevo!</p>
-          )}
+        {isHighScore(score) ? (
+          <>
+            <input
+              type="text"
+              placeholder="Ingresa tu nombre"
+              value={playerName}
+              onChange={(e) => setPlayerName(e.target.value)}
+            />
+            <button onClick={handleSave}>Guardar Puntaje</button>
+          </>
+        ) : (
+          <p className="no-highscore">
+            No lograste un puntaje alto. ¡Intenta de nuevo!
+          </p>
+        )}
 
-          <div className="modal-buttons">
-            <button onClick={onRestart}>Reiniciar</button>
-            <button onClick={handleMenu}>
-              {isHighScore ? "Menú" : "Ver Puntajes"}
-            </button>
-          </div>
+        <div className="modal-buttons">
+          <button onClick={onRestart}>Reiniciar</button>
+          <BackToMenuButton />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default GameOverModal;
