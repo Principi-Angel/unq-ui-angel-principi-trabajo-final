6192ccf35958c8105e838527fe25216eb332e72b
Principi-Angel <angel.principi@gmail.com>
2026-07-01 02:50:52 -0300

    add Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import GameBoard from "./components/GameBoard";
-
+import LeaderBoard from "./components/LeaderBoard";
+import Menu from "./components/Menu";
 
 function App() {
-
-
-  return <GameBoard></GameBoard>
-    
+  return (
+    <Router>
+      <Routes>
+        <Route path="/game" element={<GameBoard />} />
+        <Route path="/scores" element={<LeaderBoard />} />
+        <Route path="/" element={<Menu />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
