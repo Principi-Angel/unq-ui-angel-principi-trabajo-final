97c11872f83dda6490ca551cd3e0082f73e6445a
Principi-Angel <angel.principi@gmail.com>
2026-07-12 07:50:37 -0300

    add CustomButton

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,15 @@
+// ButtonCustom.jsx
+import { useNavigate } from "react-router-dom";
+import "../styles/CustomButton.css";
+
+function CustomButton({ label, route }) {
+  const navigate = useNavigate();
+
+  return (
+    <button className="custom-button" onClick={() => navigate(route)}>
+      {label}
+    </button>
+  );
+}
+
+export default CustomButton;
