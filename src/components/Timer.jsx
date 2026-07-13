7147c797db59b7cfe721462a48963df3d2145345
Principi-Angel <angel.principi@gmail.com>
2026-07-13 02:23:26 -0300

    color zones in timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,7 +4,7 @@ import { ZONES } from "../helpers/zones";
 const Timer = ({ timeLeft, colorZone}) => {
   
     return (
-      <div className={`timer ${colorZone.toLowerCase()}`}>
+      <div className={`timer ${colorZone}`}>
         {timeLeft === null 
           ? "EMPEZÁ A JUGAR" 
           : timeLeft          
--- src/helpers/zones.jsx
+++ src/helpers/zones.jsx
@@ -1,6 +1,21 @@
 export const ZONES = {
-  SAFE: "SAFE",
-  WARNING: "WARNING",
-  DANGER: "DANGER",
-  IDLE: "IDLE"
+  IDLE: "idle",
+
+  GREEN_1: "green-1",
+  GREEN_2: "green-2",
+  GREEN_3: "green-3",
+  GREEN_4: "green-4",
+  GREEN_5: "green-5",
+
+  YELLOW_1: "yellow-1",
+  YELLOW_2: "yellow-2",
+  YELLOW_3: "yellow-3",
+  YELLOW_4: "yellow-4",
+  YELLOW_5: "yellow-5",
+
+  RED_1: "red-1",
+  RED_2: "red-2",
+  RED_3: "red-3",
+  RED_4: "red-4",
+  RED_5: "red-5",
 };
